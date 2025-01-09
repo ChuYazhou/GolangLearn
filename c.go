127160e2c80b1d349b169dc42e5ad11fc3337161
chuyazhou <1534153096@qq.com>
2025-01-09 14:59:09 +0800

    增加varile.go文件

--- c.go
+++ c.go
@@ -1,10 +1,35 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Shape interface {
+	Area() float64
+}
+
+// Rectangle 定义一个结构体
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
+}
 
 func main() {
-	str := "new start!!!!"
-	fmt.Printf("Hello World,%s\n", str)
+	s := Rectangle{Width: 10.0, Height: 20.0}
+	c := Circle{Radius: 10.0}
+	fmt.Println("Area of Rectangle is:", s.Area())
+	fmt.Printf("Area of Rectangle is:%.2f\n", s.Area())
+	fmt.Println("Area of Circle is :", c.Area())
+	fmt.Printf("%+v\n", s)
+	fmt.Printf("%+v\n", c)
 }
